d0cdb44693d7ae88cca2215318b61de9119c8a78
Wildan Mubarok <willnode@wellosoft.net>
2019-08-27 23:17:37 +0700

    nicer stat

--- application/views/home/program.php
+++ application/views/home/program.php
@@ -6,10 +6,22 @@
 <div class="box-section">
 	<div class="container-label">Bio</div>
 	<div class="campus-stat">
-		<div>X Pengajar</div>
-		<div><?=$stats->students?> Mahasiswa</div>
-		<div>X Alumni</div>
-		<div>Akreditasi X</div>
+		<div class="text-center">
+			<div class="h2"><?=$stats->students?></div>
+			<div>Dosen</div>
+		</div>
+		<div class="text-center">
+			<div class="h2"><?=$stats->students?></div>
+			<div>Mahasiswa</div>
+		</div>
+		<div class="text-center">
+			<div class="h2"><?=$stats->students?></div>
+			<div>Alumni</div>
+		</div>
+		<div class="text-center">
+			<div class="h2">B</div>
+			<div>Akreditasi</div>
+		</div>
 	</div>
 </div>
 
